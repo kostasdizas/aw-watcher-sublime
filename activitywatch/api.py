8a96adc4b9a92e0265d298ab1f801e62cf7766b1
Matthew Jorgensen <matthew@jrgnsn.net>
2021-07-28 14:31:55 -0500

    Fix compatibility with aw-server-rust
    This was sent as a patch by a Jan Tojnar. Original commit message:
    
    > It expects much stricter datetime format, with explicit timezone offset.
    >
    > Let’s use `datetime.now(timezone.utc)` which unlike the naive datetime
    > returned by `datetime.utcnow()` contains a timezone.
    
    Link to patch: https://lists.sr.ht/~mjorgensen/public-inbox/patches/24051

--- activitywatch/api.py
+++ activitywatch/api.py
@@ -1,13 +1,13 @@
 import json
 import socket
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 
 from . import utils
 
 
 class ActivityWatchApi(object):
-    _last_heartbeat = datetime.now()
+    _last_heartbeat = datetime.now(timezone.utc)
     freq = 10
 
     def __init__(self):
@@ -81,7 +81,7 @@ class ActivityWatchApi(object):
             self.create_bucket(bucket_id)
 
     def heartbeat(self, bucket_id, event_data, pulsetime=30):
-        now = datetime.now()
+        now = datetime.now(timezone.utc)
 
         if not self._rate_limited(now):
             return
